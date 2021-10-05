9f3e073f3a98023f8bf2e3e72918d365c373c722
“Rachiey” <rachaelyonoff@gmail.com>
2021-10-05 17:36:34 +0100

    aattempting gif in posts

--- client/javascript/mainPage.js
+++ client/javascript/mainPage.js
@@ -1,18 +1,10 @@
 // Selectors
 const counterPost = document.getElementById("charCounterPost");
-const newPost = document.getElementById("newPost");
-const gifSearchButton = document.getElementById("giphySearchButton");
 
-// calls the Giphy api and displays the results
-// gifSearchButton.addEventListener("click", (e) => {
-//     let search = document.getElementById("gifSearch").value;
-  
-//     //for multi-word entries (replaces space with plus)
-// search = search.replace(/\s/g, "+");
-
-
-
-
+// const newPost = document.getElementById("newPost");
+// const gifSearchButton = document.getElementById("gifs");
+// const addGifButton = document.querySelector("#addGif");
+// const addPostButton = document.getElementById("journalsubmit");
 // Adding a visible remaining characters feature 
 newPost.addEventListener("input", (e) => {
     const target = e.target;
@@ -24,66 +16,87 @@ newPost.addEventListener("input", (e) => {
 });
 
 
+//Giphy feature 
+let apiKey = "EmK1vBdg1ZIGje2nKx614fyuVDlGOxjE";
+let results = document.querySelector('.giphy-search__results');
 
-// //for the GIF feature 
+function fetchGiphy() {
+    results.innerHTML = '';
+    let str = document.querySelector('#gif-search-bar').value.trim();
+    let url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&limit=1&q=${str}`;
+    fetch(url)
+        .then(resp => resp.json())
+        .then(json => {
+            let img = document.createElement('img');
+            img.src = json.data[0].images.fixed_height_downsampled.url;
+            img.style.cursor = 'pointer';
+            results.appendChild(img);
+            document.querySelector('#gif-search-bar').value = '';
+        })
+        .catch(err => {
+            console.error(err);
+        });
+}
+
+const handleGifSearch = event => {
+    if (event.key === 'Enter') {
+        fetchGiphy();
+    }
+};
+
+const addSelectedGifToPost = event => {
+    event.preventDefault();
+
+    const postContent = document.querySelector('.newPost');
+    const gifSearchModal = document.querySelector('.giphy-search');
+    const gifImage = document.querySelector('.giphy-search__results img');
+
+    if (gifImage) {
+        gifImage.style.height = '200px';
+        gifImage.style.objectFit = 'contain';
+        postContent.appendChild(gifImage);
+        gifSearchModal.style.visibility = 'hidden';
+    }
+};
+
+const searchbar = document.querySelector('.giphy-search__container input');
+const gifImageContainer = document.querySelector('.giphy-search__results');
+
+searchbar.addEventListener('keydown', handleGifSearch);
+gifImageContainer.addEventListener('click', addSelectedGifToPost);
+
+//giphy API key 
 // let APIKEY = "EmK1vBdg1ZIGje2nKx614fyuVDlGOxjE";
-// // you will need to get your own API KEY
-// // https://developers.giphy.com/dashboard/
-// document.addEventListener("DOMContentLoaded", init);
-// function init() {
-//   document.getElementById("btnSearch").addEventListener("click", ev => {
-//     ev.preventDefault(); //to stop the page reload
-//     let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
-//     let str = document.getElementById("search").value.trim();
-//     url = url.concat(str);
-//     console.log(url);
-//     fetch(url)
-//       .then(response => response.json())
-//       .then(content => {
-//         //  data, pagination, meta
-//         console.log(content.data);
-//         console.log("META", content.meta);
-//         let fig = document.createElement("figure");
-//         let img = document.createElement("img");
-//         let fc = document.createElement("figcaption");
-//         img.src = content.data[0].images.downsized.url;
-//         img.alt = content.data[0].title;
-//         fc.textContent = content.data[0].title;
-//         fig.appendChild(img);
-//         fig.appendChild(fc);
-//         let out = document.querySelector(".out");
-//         out.insertAdjacentElement("afterbegin", fig);
-//         document.querySelector("#search").value = "";
-//       })
-//       .catch(err => {
-//         console.error(err);
-//       });
-//   });
-// }
-
-
-// //hashtag feature 
-// let input, hashtagArray, container, t;
-
-// input = document.querySelector('#hashtags');
-// container = document.querySelector('.tag-container');
-// hashtagArray = [];
-
-// input.addEventListener('keyup', e => {
-//     if (e.which == 13 && input.value.length > 0) {
-//       var text = document.createTextNode(input.value);
-//       var p = document.createElement('p');
-//       container.appendChild(p);
-//       p.appendChild(text);
-//       p.classList.add('tag');
-//       input.value = '';
-      
-//       let deleteTags = document.querySelectorAll('.tag');
-      
-//       for(let i = 0; i < deleteTags.length; i++) {
-//         deleteTags[i].addEventListener('click', () => {
-//           container.removeChild(deleteTags[i]);
+//       // you will need to get your own API KEY
+//       // https://developers.giphy.com/dashboard/
+//       document.addEventListener("DOMContentLoaded", init);
+//       function init() {
+//         document.getElementById("btnSearch").addEventListener("click", ev => {
+//           ev.preventDefault(); //to stop the page reload
+//           let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
+//           let str = document.getElementById("search").value.trim();
+//           url = url.concat(str);
+//           console.log(url);
+//           fetch(url)
+//             .then(response => response.json())
+//             .then(content => {
+//               //  data, pagination, meta
+//               console.log(content.data);
+//               console.log("META", content.meta);
+//               let fig = document.createElement("figure");
+//               let img = document.createElement("img");
+//               let fc = document.createElement("figcaption");
+//               img.src = content.data[0].images.downsized.url;
+//               img.alt = content.data[0].title;
+//               fc.textContent = content.data[0].title;
+//               fig.appendChild(img);
+//               fig.appendChild(fc);
+//               let out = document.querySelector(".out");
+//               out.insertAdjacentElement("afterbegin", fig);
+//               document.querySelector("#search").value = "";
+//             })
+//             .catch(err => {
+//               console.error(err);
+//             });
 //         });
-//       }
-//     }
-// }); 
+//       };
