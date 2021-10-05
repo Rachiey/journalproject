ea1b2c0504d45be574d8a56d40de0ea3179574c6
“Rachiey” <rachaelyonoff@gmail.com>
2021-10-05 17:49:21 +0100

    accepted changes

--- client/javascript/mainPage.js
+++ client/javascript/mainPage.js
@@ -46,51 +46,6 @@ const addSelectedGifToPost = event => {
         postContent.appendChild(gifImage);
         gifSearchModal.style.visibility = 'hidden';
     }
-<<<<<<< HEAD
-};
-
-const searchbar = document.querySelector('.giphy-search__container input');
-const gifImageContainer = document.querySelector('.giphy-search__results');
-
-searchbar.addEventListener('keydown', handleGifSearch);
-gifImageContainer.addEventListener('click', addSelectedGifToPost);
-
-//giphy API key 
-// let APIKEY = "EmK1vBdg1ZIGje2nKx614fyuVDlGOxjE";
-//       // you will need to get your own API KEY
-//       // https://developers.giphy.com/dashboard/
-//       document.addEventListener("DOMContentLoaded", init);
-//       function init() {
-//         document.getElementById("btnSearch").addEventListener("click", ev => {
-//           ev.preventDefault(); //to stop the page reload
-//           let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=1&q=`;
-//           let str = document.getElementById("search").value.trim();
-//           url = url.concat(str);
-//           console.log(url);
-//           fetch(url)
-//             .then(response => response.json())
-//             .then(content => {
-//               //  data, pagination, meta
-//               console.log(content.data);
-//               console.log("META", content.meta);
-//               let fig = document.createElement("figure");
-//               let img = document.createElement("img");
-//               let fc = document.createElement("figcaption");
-//               img.src = content.data[0].images.downsized.url;
-//               img.alt = content.data[0].title;
-//               fc.textContent = content.data[0].title;
-//               fig.appendChild(img);
-//               fig.appendChild(fc);
-//               let out = document.querySelector(".out");
-//               out.insertAdjacentElement("afterbegin", fig);
-//               document.querySelector("#search").value = "";
-//             })
-//             .catch(err => {
-//               console.error(err);
-//             });
-//         });
-//       };
-=======
 });
 
 //katie messing with stuff
@@ -132,4 +87,3 @@ submitButton.addEventListener('click', e => {
   submitPost(e);
 })
 
->>>>>>> 7c9cb954bc020deeb64f639676a23fb5a07195c3
