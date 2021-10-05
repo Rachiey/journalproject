304cb4ae5b9b8d9e7c2862d198c82780e933d85f
glnkvn <golnarkavianii@gmail.com>
2021-10-05 14:03:08 +0100

    add heroku

--- client/javascript/preview.js
+++ client/javascript/preview.js
@@ -1,3 +1,5 @@
+const post_1=document.getElementById("post_1");
+const classOverview=document.getElementById("classOverview");
 
 const q="travel";
 const limit= 1;
@@ -23,3 +25,36 @@ fetch(url)
 const sent="A project is an individual or collaborative enterprise that is carefully planned and researched about by students. At schools, educational institutes and universities, a project is a research assignment - given to a student - which generally requires a larger amount of effort and more independent work than that involved in a normal essay assignment. It requires students to undertake their own fact-finding and analysis, either from library/internet research or from gathering data empirically. The written report that comes from the project is usually in the form of a dissertation, which will contain sections on the project's inception, analysis, findings and conclusions.";
 const para=document.getElementById("p");
 para.innerHTML=sent.substring(0,130);
+
+
+post_1.addEventListener('click', (e) => {
+    e.preventDefault();
+    classOverview.className="hideClass";
+
+
+    const newPost=document.getElementById("newPost");
+    const postTitle=document.createElement('h1');
+    postTitle.textContent="TiTle";
+    newPost.append(postTitle);
+    console.log(postTitle);
+    const postGif=document.createElement('img');
+    postGif.src="https://media4.giphy.com/media/9A3t72abirdtyull0m/giphy.gif?cid=2b97f132i3iuiuf7ecbnvmgfh1ruui2suyavurwvupaoz3re&rid=giphy.gif&ct=g";
+    newPost.append(postGif);
+    postGif.style.width="240px"
+    // newPost.style.backgroundColor="yellow";
+    newPost.style.margin="20vh";
+    // newPost.style.marginLeft="20vh";
+    newPost.style.border= "5px solid black";
+
+    const postForm=document.createElement('form');
+    const postInput=document.createElement('input');
+    const postLable=document.createElement('label');
+    const postComment=document.createElement('input');
+    postComment.type="textMessage";
+    postInput.type="submit";
+    newPost.append(postComment);
+    postForm.append(postInput);
+    newPost.append(postForm);
+    
+});
+
