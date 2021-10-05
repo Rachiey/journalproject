2522be0dfeb0318acf13d7e6cc20eeaf93ff1a78
glnkvn <golnarkavianii@gmail.com>
2021-10-05 18:53:25 +0100

    add style for showing only one post on screen

--- client/javascript/preview.js
+++ client/javascript/preview.js
@@ -35,7 +35,7 @@ post_2.addEventListener('click', (e) => {
     .then(data => setValues(data.all))
 
     function setValues(data){
-        console.log(data[1].post);
+        // console.log(data[1].post);
     // for (let i = data.length - 1; i >= 0; i--) {
         const art=document.getElementById('addPosts');
         const sector=document.createElement('section');
@@ -83,30 +83,76 @@ post_1.addEventListener('click', (e) => {
     e.preventDefault();
     classOverview.className="hideClass";
 
+    fetch('http://localhost:3000/posts')
+    .then(obj => obj.json())
+    .then(data => {
+            
+        const art=document.getElementById('newPost');
+        const sector=document.createElement('section');
+        const newHeader=document.createElement('header');
+        const newH1=document.createElement('h1');
+        const newH2=document.createElement('h2');
+        const newImg=document.createElement('img');
+        const newMsg=document.createElement('p');
+        newH1.textContent=data.all[0].post;
+        newH2.textContent=data.all[0].location;
+        newMsg.textContent=data.all[0].message;
+        newImg.src=data.all[0].gif;
 
-    const newPost=document.getElementById("newPost");
-    const postTitle=document.createElement('h1');
-    postTitle.textContent="TiTle";
-    newPost.append(postTitle);
-    console.log(postTitle);
-    const postGif=document.createElement('img');
-    postGif.src="https://media4.giphy.com/media/9A3t72abirdtyull0m/giphy.gif?cid=2b97f132i3iuiuf7ecbnvmgfh1ruui2suyavurwvupaoz3re&rid=giphy.gif&ct=g";
-    newPost.append(postGif);
-    postGif.style.width="240px"
-    // newPost.style.backgroundColor="yellow";
-    newPost.style.margin="20vh";
-    // newPost.style.marginLeft="20vh";
-    newPost.style.border= "5px solid black";
+        newImg.className="gifs";
+        newHeader.className="heads";
+        newH1.className="titles";
+        newH2.className="locs";
+        sector.className="selection";
+        newMsg.className="p";
 
-    const postForm=document.createElement('form');
-    const postInput=document.createElement('input');
-    const postLable=document.createElement('label');
-    const postComment=document.createElement('input');
-    postComment.type="textMessage";
-    postInput.type="submit";
-    newPost.append(postComment);
-    postForm.append(postInput);
-    newPost.append(postForm);
+        newHeader.append(newH2);
+        newHeader.append(newH1);
+        sector.append(newHeader);
+        sector.append(newImg);
+        sector.append(newMsg);
+        art.append(sector);
+
+        art.style.width="50vw";
+        art.style.marginLeft="25vw";
+        art.style.marginTop="15vh";
+        newH1.style.width="calc((0.6 * 100vw)/2)";
+        newImg.style.width="200px";
+        newImg.style.height="200px";
+        newImg.style.marginLeft="calc((calc(100vw/2) - 200px)/2)";
+        art.style.border="5px solid black";
+        // art.style.backgroundColor="blue";
+
+      
+    })
+
+
+
+
+
+    // const newPost=document.getElementById("newPost");
+    // const postTitle=document.createElement('h1');
+    // postTitle.textContent="TiTle";
+    // newPost.append(postTitle);
+    // console.log(postTitle);
+    // const postGif=document.createElement('img');
+    // postGif.src="https://media4.giphy.com/media/9A3t72abirdtyull0m/giphy.gif?cid=2b97f132i3iuiuf7ecbnvmgfh1ruui2suyavurwvupaoz3re&rid=giphy.gif&ct=g";
+    // newPost.append(postGif);
+    // postGif.style.width="240px"
+    // // newPost.style.backgroundColor="yellow";
+    // newPost.style.margin="20vh";
+    // // newPost.style.marginLeft="20vh";
+    // newPost.style.border= "5px solid black";
+
+    // const postForm=document.createElement('form');
+    // const postInput=document.createElement('input');
+    // const postLable=document.createElement('label');
+    // const postComment=document.createElement('input');
+    // postComment.type="textMessage";
+    // postInput.type="submit";
+    // newPost.append(postComment);
+    // postForm.append(postInput);
+    // newPost.append(postForm);
     
 });
 
